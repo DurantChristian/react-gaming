e3d9d2db90cd44001a2aa9df03c30543da684dba
DurantChristian <cdd7@email.sc.edu>
2025-04-12 20:51:23 -0400

    edited reviewcard.jsx

--- app-reactgaming/src/components/Reviewcard.jsx
+++ app-reactgaming/src/components/Reviewcard.jsx
@@ -7,7 +7,7 @@ const ReviewCard = (props) => {
       <img src={`http://localhost:3001/images/${props.img_name}`} alt={props.title} />
       <p><strong>Genre:</strong> {props.genre}</p>
       <p>{props.about}</p>
-      <p><strong>Release Year:</strong> {release_year}</p>
+      <p><strong>Release Year:</strong> {props.release_year}</p>
       <p><strong>Rating:</strong> {props.rating}/10</p>
       <a href={props.external_link} target="_blank" rel="noopener noreferrer">
         Read More
